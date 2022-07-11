060674856ccdfab076ee6b4891c21c3084b2c3b3
patipan-patisampita <patipan.patisampita@gmail.com>
2022-07-11 16:05:05 +0700

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http')
 const msg = require('./message.js')
 
 const host = "localhost"
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' })
     res.write(`${msg.message}`)

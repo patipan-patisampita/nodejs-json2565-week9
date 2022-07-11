a77438c14413bfbb274e7377c75f83e34d3bc22d
patipan-patisampita <patipan.patisampita@gmail.com>
2022-07-11 16:52:30 +0700

    update

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const http = require('http')
 const msg = require('./message.js')
+const {loadUser}  = require('./user_models')
 
 const host = "localhost"
 const PORT = process.env.PORT || 5000
+
 const server = http.createServer((req, res) => {
+    const jsonContent = JSON.stringify(loadUser())
+
     res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.write(`${msg.message}`)
+    res.write(`${jsonContent}`)
     res.end()
 })
 
